220ad8040b3421857d7e6325a282c7582519e61f
Levbitz <maxinosamx@gmail.com>
2020-11-20 08:36:03 +0530

    second

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 // javascript for index.html
-const container = document.querySelector('.blogs');
-const searchForm = document.querySelector('.search');
+const container = document.querySelector(".blogs");
+const searchForm = document.querySelector(".search");
 
 const renderPosts = async (term) => {
   console.log(term);
-  let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+  let uri = "https://github.com/Levbitz/jsonserve/blob/main/data/db.json";
   if (term) {
-    uri += `&q=${term}`
+    uri += `&q=${term}`;
   }
 
   const res = await fetch(uri);
   const posts = await res.json();
   console.log(posts);
 
-  let template = '';
-  posts.forEach(post => {
+  let template = "";
+  posts.forEach((post) => {
     template += `
       <div class="post">
         <h2>${post.title}</h2>
@@ -22,16 +22,16 @@ const renderPosts = async (term) => {
         <p>${post.body.slice(0, 200)}...</p>
         <a href="/details.html?id=${post.id}">Read more</a>
       </div>
-    `
+    `;
   });
 
   container.innerHTML = template;
-}
+};
 
 // search
-searchForm.addEventListener('submit', async (e) => {
+searchForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   renderPosts(searchForm.term.value.trim());
-})
+});
 
-window.addEventListener('DOMContentLoaded', () => renderPosts());
+window.addEventListener("DOMContentLoaded", () => renderPosts());
